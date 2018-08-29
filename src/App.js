0106c5f8ba01a37cb8da28e255d708f3306f1bf5
Sharan-ram <sharanram1993@gmail.com>
2018-08-30 02:28:38 +0530

    style the page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import Kisi from "kisi-client"
 // import functions
 import configureStore from './configureStore'
@@ -18,15 +19,18 @@ const API_KEY = '94c2056abb993b570517f2d3a89c9b5a'
 kisiClient.setLoginSecret(API_KEY)
 
 const store = configureStore()
+const theme = createMuiTheme()
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Locks} />
-      </Switch>
-    </Router>
-  </Provider>
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Locks} />
+        </Switch>
+      </Router>
+    </Provider>
+  </MuiThemeProvider>
 )
 
 export default App
--- src/components/Locks/index.js
+++ src/components/Locks/index.js
@@ -2,6 +2,16 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+// import material ui components
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Typography from '@material-ui/core/Typography'
 // import actions
 import {
   startFetchLocks,
@@ -14,6 +24,23 @@ import {
   getLocksArray,
   unlockStatus
 } from '../../redux/reducers/locks'
+
+const styles = {
+  Container: {
+    margin: 50,
+    overflow: 'auto'
+  },
+  Spinner: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    top:0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto'
+  }
+}
 
 class LocksContainer extends Component {
   static propTypes = {
@@ -38,47 +65,51 @@ class LocksContainer extends Component {
   renderLocks() {
     const { locksArray, unlockStatus } = this.props
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>
-              Name
-            </th>
-            <th>
-              Select Unlock
-            </th>
-            <th>
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {locksArray.map(lockObj => {
-            const { id } = lockObj
-            const status = unlockStatus[id]
-            return (
-              <tr key={lockObj.id}>
-                <td>
-                  {lockObj.name}
-                </td>
-                <td>
-                  <button
-                    type='button'
-                    onClick={this.handleUnlock}
-                    value={lockObj.id}
-                    disabled={this.isButtonDisabled(status)}
-                  >
-                    Unlock
-                  </button>
-                </td>
-                <td>
-                  {status ? status : ''}
-                </td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Name
+              </TableCell>
+              <TableCell>
+                Select Unlock
+              </TableCell>
+              <TableCell>
+                Status
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {locksArray.map(lockObj => {
+              const { id, name } = lockObj
+              const status = unlockStatus[id]
+              console.log("status inside render", status);
+              return (
+                <TableRow key={id}>
+                  <TableCell>
+                    {name}
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant='contained'
+                      color='secondary'
+                      onClick={(e) => { this.handleUnlock(e, id) }}
+                      id={id}
+                      disabled={this.isButtonDisabled(status)}
+                    >
+                      Unlock
+                    </Button>
+                  </TableCell>
+                  <TableCell>
+                    {this.getStatus(status)}
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 
@@ -90,19 +121,39 @@ class LocksContainer extends Component {
     return false
   }
 
-  handleUnlock(e) {
-    const { value: lockId } = e.target
+  handleUnlock(e, lockId) {
     const { unlockDoor } = this.props
     unlockDoor(lockId)
+  }
+
+  getStatus(status) {
+    console.log("status inside getStatus", status);
+    if (status === 'inProgress') {
+      return <CircularProgress color="secondary" />
+    }
+    if (status) return status
+    return ''
   }
 
   render() {
     const { isLocksfetching, errMsg } = this.props
     return (
-      <div>
+      <div style={styles.Container}>
         {isLocksfetching ?
-          'Fetching Locks pls wait' :
-          this.renderLocks()
+          <div style={styles.Spinner}>
+            <CircularProgress color="secondary" />
+          </div> :
+          <div>
+            <Typography
+              variant='display1'
+              paragraph
+              gutterBottom
+            >
+              Showing a list of locks, click on the unlock button
+                to unlock the door and to update the status
+            </Typography>
+            {this.renderLocks()}
+          </div>
         }
         {!isLocksfetching && errMsg &&
           <div>
